bf0a7f9d133883e77a82cead51f09faf5d3f4e03
starwiz-7 <aryan2019@iiitkottayam.ac.in>
2021-10-21 14:38:01 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -5,14 +5,33 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-
+import { Box, useColorModeValue, Spinner, Center } from '@chakra-ui/react';
+import { COLORS } from './colors';
 import Home from '../src/pages/Home/home';
 const Room = lazy(() => import('../src/pages/Room/room'));
 
+const Loader = () => {
+  return (
+    <Center width="100%" height="100vh">
+      <Box
+        px={8}
+        py={5}
+        mx="auto"
+        pb={20}
+        backgroundColor={useColorModeValue(COLORS.white, COLORS.dark)}
+      >
+        <Spinner
+          size="xl"
+          color={useColorModeValue(COLORS.dark, COLORS.white)}
+        />
+      </Box>
+    </Center>
+  );
+};
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Switch>
           <Route
             exact
