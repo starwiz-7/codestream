816a12a353ed8cca3f09bb09e96d1f9dd8dd92be
starwiz-7 <aryan2019@iiitkottayam.ac.in>
2021-11-09 22:51:03 +0530

    Added features section and changed the landing page images

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -12,13 +12,46 @@ import {
 import { FcCheckmark } from 'react-icons/fc';
 
 // Replace test data with your own
-const features = Array.apply(null, Array(8)).map(function (x, i) {
-  return {
-    id: i,
-    title: 'Lorem ipsum dolor sit amet',
-    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
-  };
-});
+// const features = Array.apply(null, Array(8)).map(function (x, i) {
+//   return {
+//     id: i,
+//     title: 'Lorem ipsum dolor sit amet',
+//     text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+//   };
+// });
+
+const features = [
+  {
+    id: 1,
+    title: 'Realtime - collaborative',
+    text: 'Users can collaborate on their code',
+  },
+  {
+    id: 2,
+    title: 'Languages',
+    text: 'Codestream supports multiple language highlighting',
+  },
+  {
+    id: 3,
+    title: 'Compiler',
+    text: 'It has an embedded compiler which can compile your code',
+  },
+  {
+    id: 4,
+    title: 'Chat',
+    text: 'Users can chat while they collaborate',
+  },
+  {
+    id: 5,
+    title: 'Zen mode',
+    text: 'Switch to zen mode(full screen) for a better editing experience',
+  },
+  {
+    id: 6,
+    title: 'Responsive',
+    text: 'Start collaborating through your mobile',
+  },
+];
 
 export default function GridListWithHeading() {
   return (
@@ -27,15 +60,10 @@ export default function GridListWithHeading() {
         <chakra.h1 fontSize={48} fontWeight={'bold'}>
           Features
         </chakra.h1>
-        <Text color={'gray.600'} fontSize={'xl'}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua.
-        </Text>
       </Stack>
 
       <Container maxW={'6xl'} mt={10}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10}>
           {features.map(feature => (
             <HStack key={feature.id} align={'top'}>
               <Box color={'green.400'} px={2}>
